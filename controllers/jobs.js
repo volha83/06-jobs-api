381962f63579feb61fcc33b08742eaa5172e1377
volha83 <170294328+volha83@users.noreply.github.com>
2026-01-31 01:07:48 -0500

    Implement CRUD operations for Jobs with DB integration

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,21 +3,58 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  res.send("get single job");
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: req.user.userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or position fields can not be empty");
+  }
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: req.user.userId },
+    req.body,
+    { new: true, runValidators: true },
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const { id: jobId } = req.params;
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: req.user.userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 module.exports = {
